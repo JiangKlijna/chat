7567c937f2c917a6b41310f074ad4e78dcbf3fce
jiangKlijna <leil729564090@outlook.com>
2017-11-12 15:01:16 +0800

    :fire:modify the initial style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="hundred">
     <router-view/>
   </div>
 </template>
@@ -11,12 +11,16 @@ export default {
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    font-family: consolas;
+}
+html, body, .hundred {
+    width: 100%;
+    height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

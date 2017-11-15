3839ea97b4586a94b6aac79451b49174416fd949
jiangKlijna <leil729564090@outlook.com>
2017-11-15 20:44:48 +0800

    :fire:主页面移动端适配

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
-  <div id="app" class="hundred">
-    <router-view/>
-  </div>
+    <div id="app" class="hundred">
+        <div id="main" v-bind:class="main_class" v-bind:style="{'margin-top': main_marginTop + 'px'}">
+            <router-view/>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+    name: 'app',
+    data: function () {
+        return {
+            main_class: null,
+            main_marginTop: 60
+        }
+    },
+    methods: {
+        onResize: function () {
+            if (window.innerWidth < 780) { // 调整为移动端样式
+                this.main_class = "hundred";
+            } else { // 调整为pc端样式
+                this.main_class = "pc_main";
+                this.main_marginTop = ((window.innerWidth-780) / (window.screen.width-780)) * 60;
+            }
+        }
+    },
+    mounted: function () {
+        window.onresize = this.onResize;
+        window.onresize();
+    }
 }
 </script>
 
@@ -22,5 +44,18 @@ html, body, .hundred {
 }
 #app {
   text-align: center;
+  background-color: #f1f1f1;
+}
+#main {
+    border-radius: 4px;
+    background-color: white;
+    box-shadow: 0 0 8px rgba(0,0,0,.1);
+    vertical-align: middle;
+    display: inline-block;
+}
+.pc_main {
+    width: 600px;
+    height: 800px;
+    margin: 0 auto;
 }
 </style>

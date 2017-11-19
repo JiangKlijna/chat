42b783232bcdab39a5faacdbcf1a8e05a28c7b40
jiangKlijna <leil729564090@outlook.com>
2017-11-19 15:37:15 +0800

    :fire:add theme.vue

---
+++ src/components/Theme.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Theme',
+    data: function () {
+        return {
+            colors: []
+        }
+    },
+    mounted: function () {
+        window.theme = this;
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Theme from '@/components/Theme'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [
+        {path: '/', name: 'Hello', component: Hello},
+        {path: '/theme', name: 'Theme', component: Theme},
+    ]
 })

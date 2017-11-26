331b4fce161312be1593daef0b71f651eb835630
jiangKlijna <leil729564090@outlook.com>
2017-11-26 21:26:16 +0800

    :fire:theme.vue content

--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-
+    <div id="theme" class="mdui-row-xs-1 mdui-row-sm-2 mdui-row-md-3">
+        <div v-for="color in colors" v-bind:class="'mdui-col mdui-text-color-'+color">
+            <label class="mdui-radio mdui-m-b-1">
+                <input type="radio" name="doc-theme-accent" v-bind:value="color">
+                <i class="mdui-radio-icon"></i>{{color}}
+            </label>
+        </div>
     </div>
 </template>
 
@@ -13,11 +18,19 @@ export default {
         }
     },
     mounted: function () {
+        this.colors = window.R.colors;
         window.theme = this;
     }
 }
 </script>
 
 <style>
-
+    #theme {
+        height: 80%;
+        padding: 10px;
+        overflow: scroll;
+    }
+    .mdui-col {
+        text-align: left;
+    }
 </style>

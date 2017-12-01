e7ae479c315c6f1fee92546d25cc258c055a915b
jiangKlijna <leil729564090@outlook.com>
2017-12-01 20:21:14 +0800

    :fire:rewrite routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,16 @@
 
 // restful api
 
-module.exports = function (app) {
-    app.get('/test', function (req, res) {
-        res.send('test')
-    })
+// generator
+const gen = function (method, path, fun) {
+    fun.setting = app => eval(`app.${method}('${path}', fun)`);
+    return fun;
 }
+
+const routes = [
+    gen('get', '/test', function (req, res) {
+        res.send('test')
+    }),
+]
+
+module.exports = app => {for (var r of routes) r.setting(app)};

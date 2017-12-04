edcb62f63cb1a02290263ecf5f0ec1472a304cc8
jiangKlijna <leil729564090@outlook.com>
2017-12-04 20:22:48 +0800

    :fire:确定接口

--- server/routes.js
+++ server/routes.js
@@ -7,9 +7,35 @@ const gen = function (method, path, fun) {
     return fun;
 }
 
+// routes
 const routes = [
-    gen('get', '/test', function (req, res) {
-        res.send('test')
+    // 登陆
+    gen('post', '/user/sign/in', function (req, res) {
+        res.send('signin')
+    }),
+    // 注册
+    gen('post', '/user/sign/up', function (req, res) {
+        res.send('signup')
+    }),
+    // 搜索
+    gen('post', '/user/search', function (req, res) {
+        res.send('search')
+    }),
+    // 修改一个用户信息，只有自己才能修改
+    gen('post', '/user/update', function (req, res) {
+        res.send('update')
+    }),
+    // 获得一个用户的详细信息
+    gen('post', '/user/info', function (req, res) {
+        res.send('info')
+    }),
+    // 获得消息列表
+    gen('post', '/message/list', function (req, res) {
+        res.send('list')
+    }),
+    // 删除一个会话的所有消息
+    gen('post', '/message/delete', function (req, res) {
+        res.send('delete')
     }),
 ]
 

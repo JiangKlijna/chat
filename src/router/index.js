c02ab212c779ee93e8d86b6c9f9558daeb2a2502
jiangKlijna <leil729564090@outlook.com>
2017-12-05 19:57:58 +0800

    :fire:add Login.vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Message from '@/components/Message'
 import Theme from '@/components/Theme'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -9,5 +10,6 @@ export default new Router({
     routes: [
         {path: '/', name: 'Message', component: Message},
         {path: '/theme', name: 'Theme', component: Theme},
+        {path: '/login', name: 'Login', component: Login},
     ]
 })

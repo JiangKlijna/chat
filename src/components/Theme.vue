d0805ace217c7f3954ad039a0d84f9bb0d3030d0
jiangKlijna <leil729564090@outlook.com>
2017-12-31 19:38:11 +0800

    :fire:move toolbar

--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -1,17 +1,22 @@
 <template>
-    <div id="theme" class="mdui-row-xs-1 mdui-row-sm-2 mdui-row-md-3">
-        <div v-for="color in colors" v-bind:class="'mdui-col mdui-text-color-'+color">
-            <label class="mdui-radio mdui-m-b-1">
-                <input type="radio" name="doc-theme-accent" v-on:click="onClickColor(color)" v-bind:value="color" v-bind:checked="current === color">
-                <i class="mdui-radio-icon"></i>{{color}}
-            </label>
+    <div id="theme">
+        <ToolBar class="app_title"/>
+        <div class="mdui-row-xs-1 mdui-row-sm-2 mdui-row-md-3">
+            <div v-for="color in colors" v-bind:class="'mdui-col mdui-text-color-'+color">
+                <label class="mdui-radio mdui-m-b-1">
+                    <input type="radio" name="doc-theme-accent" v-on:click="onClickColor(color)" v-bind:value="color" v-bind:checked="current === color">
+                    <i class="mdui-radio-icon"></i>{{color}}
+                </label>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import ToolBar from './ToolBar'
 export default {
     name: 'Theme',
+    components: {ToolBar},
     data: function () {
         return {
             current: null,

31d93365a93eed5cbee724521ac963e5405b40a5
jiangKlijna <leil729564090@outlook.com>
2018-01-10 20:23:42 +0800

    :fire:adjust login style

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,18 +2,20 @@
     <div id="login">
         <ToolBar title="登陆"/>
         <div id="login_body">
+            <br>
             <div class="mdui-textfield mdui-textfield-floating-label">
                 <i class="mdui-icon material-icons">account_circle</i>
                 <label class="mdui-textfield-label">账号:</label>
                 <input class="mdui-textfield-input" v-model:value="username"/>
             </div>
+            <br>
             <div class="mdui-textfield mdui-textfield-floating-label">
                 <i class="mdui-icon material-icons">lock</i>
                 <label class="mdui-textfield-label">密码:</label>
                 <input class="mdui-textfield-input" v-model:value="password" type="password"/>
             </div>
-            <br>
-            <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple" v-on:click="onClickLogin">登 陆</button>
+            <br><br>
+            <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-shadow-8" v-on:click="onClickLogin">登 陆</button>
         </div>
     </div>
 </template>

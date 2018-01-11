14f1a98559e9c1eb7691ceaf79b1713c111b313d
jiangKlijna <leil729564090@outlook.com>
2018-01-11 20:52:04 +0800

    :fire:add regist.vue style

--- src/components/Regist.vue
+++ src/components/Regist.vue
@@ -2,6 +2,20 @@
     <div id="regist">
         <ToolBar title="注册"/>
         <div id="regist_body">
+            <br>
+            <div class="mdui-textfield mdui-textfield-floating-label">
+                <i class="mdui-icon material-icons">face</i>
+                <label class="mdui-textfield-label">昵称:</label>
+                <input class="mdui-textfield-input" v-model:value="username"/>
+            </div>
+            <br>
+            <div class="mdui-textfield mdui-textfield-floating-label">
+                <i class="mdui-icon material-icons">lock</i>
+                <label class="mdui-textfield-label">密码:</label>
+                <input class="mdui-textfield-input" v-model:value="password" type="password"/>
+            </div>
+            <br><br>
+            <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-shadow-8" v-on:click="onClickLogin">登 陆</button>
         </div>
     </div>
 </template>
@@ -20,5 +34,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+    #regist_body {
+        text-align: left;
+    }
 </style>

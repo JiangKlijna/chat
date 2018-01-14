3a0dbaec811e9f8088e8c9b101131806f6a26b00
jiangKlijna <leil729564090@outlook.com>
2018-01-14 19:20:23 +0800

    :fire:add regist_show

--- src/components/Regist.vue
+++ src/components/Regist.vue
@@ -17,6 +17,10 @@
             <br><br>
             <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-shadow-8" v-on:click="onClickLogin">登 陆</button>
         </div>
+        <div id="regist_show">
+            <p><img v-bind:src="imgurl"></p>
+            <p>{{username}}</p>
+        </div>
     </div>
 </template>
 
@@ -27,6 +31,25 @@ export default {
     components: {ToolBar},
     data () {
         return {
+            imgurl: null,
+            username: '',
+            password: ''
+        }
+    },
+    watch: {
+        username: function (n, o) {
+            this.username = n.trim();
+            if (this.username !== n) return;
+            if (n === '') {
+                this.imgurl = null;
+                return;
+            }
+            if (n[0] === o[0]) return;
+            this.imgurl = new mdAvatar({size: 80, text: n}).toDataURL();
+        }
+    },
+    methods: {
+        onClickLogin: function () {
         }
     }
 }

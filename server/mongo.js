aebdad83fd659181af6c41c046c33aba79e39c80
jiangKlijna <leil729564090@outlook.com>
2018-01-20 22:06:54 +0800

    :fire:add increse id

--- server/mongo.js
+++ server/mongo.js
@@ -7,6 +7,21 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUrl, {useMongoClient:true});
 
+// userid increase
+const Userid_id = "jiangKlijna"
+let UseridSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
+    seq: {type: Number, default: 1}
+});
+let Userid = mongoose.model('Userid', UseridSchema);
+// 如果ui已经存在， 不存在则创建一个
+Userid.findById(Userid_id, function (err, ui) {
+    console.log("19", err, ui);
+    if (err) return console.log(err);
+    if (ui) return;
+    new Userid({_id: Userid_id}).save();
+});
+
 // User
 let UserSchema = new mongoose.Schema({
     userid: {type: Number, index: {unique: true}},
@@ -14,18 +29,28 @@ let UserSchema = new mongoose.Schema({
     password: String,
     add_time: Date
 });
+// save前设置自增id
+UserSchema.pre('save', function(next) {
+    let u = this;
+    Userid.findByIdAndUpdate({_id: Userid_id}, {$inc: {seq: 1}}, function(err, counter) {
+        console.log("36", err, counter);
+        if(err) return next(err);
+        u.userid = counter.seq;
+        next();
+    });
+});
 
 module.exports = {
     User: mongoose.model('User', UserSchema),
-}
+};
 
 // test
 if(module === require.main) {
-    let u = new module.exports.User({userid: 1, username: "cj", password: "123", add_time:new Date()});
-    u.save(function (err, User) {
-      console.log(User)
-      if (err) return console.error(err);
+    let User = mongoose.model('User', UserSchema);
+    let u = new User({userid: 3, username: "cj2", password: "123", add_time:new Date()});
+    u.save(function (err) {
+        if(err) console.log(err)
     });
-    console.log(u)
+    // console.log(u)
     //mongoose.disconnect(m => console.log(m));
 }

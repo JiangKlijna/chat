44da3dd0a6aea23bf4232e4a8ab823da3b644313
jiangKlijna <leil729564090@outlook.com>
2018-03-06 17:55:05 +0800

    :fire:adjust style

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,18 +1,12 @@
 <template>
-    <div id="menu" class="app_title mdui-toolbar mdui-color-theme">
+    <div id="menu" class="app_title mdui-toolbar mdui-color-theme mdui-ripple">
         <a v-on:click="onBack" v-show="!isRoot" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">arrow_back</i></a>
         <span class="mdui-typo-title">{{title}}</span>
         <div class="mdui-toolbar-spacer"></div>
         <a v-on:click="onClickIcon('/')" v-show="!isRoot" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">message</i></a>
         <a v-on:click="onClickIcon('/theme')" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">color_lens</i></a>
-        <a class="mdui-btn mdui-btn-icon" mdui-menu="{target: '#menu_item'}"><i class="mdui-icon material-icons">more_vert</i></a>
-        <ul id="menu_item" class="mdui-menu mdui-color-theme">
-            <li class="mdui-menu-item mdui-ripple"><i class="mdui-menu-item-icon mdui-icon material-icons">group</i>好友</li>
-            <li class="mdui-divider"></li>
-            <li class="mdui-menu-item mdui-ripple"><i class="mdui-menu-item-icon mdui-icon material-icons">person</i>我的</li>
-            <li class="mdui-divider"></li>
-            <li class="mdui-menu-item mdui-ripple"><i class="mdui-menu-item-icon mdui-icon material-icons">settings</i>设置</li>
-        </ul>
+        <a v-on:click="onClickIcon('/search')" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">search</i></a>
+        <a v-on:click="onClickIcon('/person')" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">person</i></a>
     </div>
 </template>
 

fdd99f1c29bfff51934a1a5c0e354e8ff514e09f
jiangKlijna <leil729564090@outlook.com>
2018-04-12 20:41:05 +0800

    :fire:add chat list

--- server/methods.js
+++ server/methods.js
@@ -65,11 +65,59 @@ const ChatService = {
      */
     record: (fromUserid, toUserid, content) => new Promise((resolve, reject) => {
         let m = new Message({fromUserid, toUserid, content, time:new Date()});
-        u.save(function (err) {
+        m.save(function (err) {
             if(err) reject(err);
             else resolve(m);
         })
     }),
+    /**
+     * 获得聊天列表
+     * @param toUser
+     * @returns {Promise<List<Message>>}
+     * {$or: [{toUserid: userid}, {fromUserid: userid}]}
+     */
+    list: (userid) => new Promise(async (resolve, reject) => {
+        try {
+            let map = new Map();
+            let list1 = await Message.aggregate().match({toUserid: userid}).sort({'_id': -1}).group({
+                _id: {fromUserid: "$fromUserid"},
+                toUserid: {$push: "$toUserid"},
+                fromUserid: {$push: "$fromUserid"},
+                time: {$push: "$time"},
+                content: {$push: "$content"}
+            }).exec();
+            let list2 = await Message.aggregate().match({fromUserid: userid}).sort({'_id': -1}).group({
+                _id: {toUserid: "$toUserid"},
+                toUserid: {$push: "$toUserid"},
+                fromUserid: {$push: "$fromUserid"},
+                time: {$push: "$time"},
+                content: {$push: "$content"}
+            }).exec();
+            let each = e => {
+                delete e._id;
+                e.fromUserid = e.fromUserid[0];
+                e.toUserid = e.toUserid[0];
+                e.time = e.time[0].getTime();
+                e.content = e.content[0];
+                // 遍历过程中
+                let tagUserid = (e.fromUserid === userid) ? e.toUserid : e.fromUserid;
+                if (map.hasOwnProperty(tagUserid)) { // 已存在
+                    let tag = map[tagUserid];
+                    // 比较时间
+                    if (e.time > tag.time) map[tagUserid] = e;
+                } else { // 不存在
+                    map[tagUserid] = e;
+                }
+            };
+            list1.forEach(each);
+            list2.forEach(each);
+            let result = [];
+            for (let v in map) result.push(map[v]);
+            resolve(result);
+        } catch (err) {
+            reject(err);
+        }
+    })
 };
 
 module.exports = {
@@ -80,9 +128,18 @@ module.exports = {
 // test
 if(module === require.main) {
     (async () => {
-        let u = await UserService.verification('1', 'update');
-        console.log("await", u)
-        let r = await UserService.update(1, {username: 'update'});
-        console.log("await", r)
+        try {
+            let u = await UserService.verification('1', 'test');
+            console.log("verification", u);
+            let r = await UserService.update(1, {username: 'test', password: 'test'});
+            console.log("update", r);
+
+            let m = await ChatService.record(8, 6, "test");
+            console.log("record", m);
+            let l = await ChatService.list(8);
+            console.log("list", l);
+        }catch (e) {
+            console.log(e)
+        }
     })()
 }

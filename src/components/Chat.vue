46a867aa9d6eee43c45c7aa105d229e9cb20d9d1
jiangKlijna <leil729564090@outlook.com>
2018-04-28 19:03:53 +0800

    :fire:add chat input

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,7 +1,14 @@
 <template>
     <div id="chat">
-        <ToolBar title="聊天"/>
+        <ToolBar v-bind:title="username"/>
         <div id="chat_body">
+            <div class="mdui-textfield">
+                <input id="chat_text" class="mdui-textfield-input"/>
+            </div>
+            <button id="chat_btn" class="mdui-textfield-close mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">send</i></button>
+            <div id="chat_list">
+
+            </div>
         </div>
     </div>
 </template>
@@ -13,12 +20,14 @@ export default {
     components: {ToolBar},
     data () {
         return {
-
+            username: null,
         }
     },
     mounted: function () {
+        app.user = {userid:8,username:'skt'};
         // 如果未登陆则跳转到login
         if (app.user === null) this.$router.push('/login');
+        this.username = app.user.username;
         console.log(this.$route.params.userid)
     }
 }
@@ -26,5 +35,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+    .mdui-textfield {
+        padding-top: 0;
+        float: left;
+        width: 100%;
+    }
+    #chat_body {
+        position: relative;
+    }
+    #chat_list {
+        width: 100%;
+    }
+    #chat_text {
+        padding-left: 10px;
+    }
+    #chat_btn {
+        position: absolute;
+        right: 0;
+    }
 </style>
